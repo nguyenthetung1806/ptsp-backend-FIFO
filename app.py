9ac159597d6ac91778413175e956dad0bd9d832e
DESKTOP-1QO3O5N\MyLaptop <nguyenthetung1806>
2018-08-20 01:27:39 +0700

    'asdasd'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, Response, send_from
 from openpyxl import Workbook, load_workbook
 from io import BytesIO
 import pandas as pd
+import numpy as np
 from flask_cors import CORS, cross_origin
 
 wb = Workbook()
@@ -34,28 +35,37 @@ def api():
     # Load a sheet into a DataFrame by name: df1
     df0 = xl.parse(xl.sheet_names[0])
 
-    df1 = df0[['SYMBOL','SIDE','QTTY','PRICE']].copy()
-    df_sell = df1.loc[df1['SIDE'] == 'S']
-    df_buy = df1.loc[df1['SIDE'] == 'B']
+    df1 = df0[['TIME_EXEC','SYMBOL','SIDE','QTTY','PRICE']].copy()
 
-    df1["TotalPrice"] = 0
+    #repeat rows
+    df1 = df1.loc[df1.index.repeat(df1['QTTY'])]
+    #group by index with transform for date ranges
+    df1 = df1.sort_values(by='TIME_EXEC')
+    #unique default index
+    df1 = df1.reset_index(drop=True)
+    df1['QTTY'] = 1
 
+    df1["Matched_Price"] = 0
     inventory = []
-    inventory_side =  df1.iloc[0]['SIDE']
+    inventory_side = df1.iloc[0]['SIDE']
     for index, row in df1.iterrows():
-        for i in range(row['QTTY']):
-            if inventory_side == 'No_side':
-                inventory_side = row['SIDE']
-            if inventory_side == (row['SIDE']):
-                inventory.append( row['PRICE'] )
-            else:
-                total_price_last = inventory[0] + df1.loc[index, 'TotalPrice']
-                df1.loc[index, 'TotalPrice'] = total_price_last
-                del inventory[0]
-                if len(inventory) == 0:
-                    inventory_side = 'No_side'
+        if inventory_side == 'No_side':
+            inventory_side = row['SIDE']
+        if inventory_side == (row['SIDE']):
+            inventory.append( row['PRICE'] )
+        else:
+            df1.loc[index, 'Matched_Price'] = inventory[0]
+            del inventory[0]
+            if len(inventory) == 0:
+                inventory_side = 'No_side'
 
-    df1['Average Matched Price'] = df1['TotalPrice'] / df1['QTTY']
+    conditions = [
+        (df1['SIDE'] == 'S') & (df1['Matched_Price'] != 0),
+        (df1['SIDE'] == 'B') & (df1['Matched_Price'] != 0)]
+    choices = [(df1['PRICE'] - df1['Matched_Price']), (df1['Matched_Price'] - df1['PRICE'])]
+    df1['Realized_Profit'] = np.select(conditions, choices, default=0)
+
+
 
 
     # Write your DataFrame to a file
