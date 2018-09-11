54e2cf4f8aa39ab1732dd479c5ccecaf1773b2ba
DESKTOP-1QO3O5N\MyLaptop <nguyenthetung1806>
2018-09-11 23:19:32 +0700

    'dsdsf'

--- app.py
+++ app.py
@@ -30,66 +30,62 @@ def api():
 
     writer = pd.ExcelWriter('Result.xlsx', engine='xlsxwriter')
 
-    # Read file Data Môi Giới
-    DS_MG = "./Data Môi Giới.xlsx"
-    DS_Hang = "./DS Hạng.xlsx"
-    xl_DS_MG = pd.ExcelFile(DS_MG)
-    xl_DS_Hang = pd.ExcelFile(DS_Hang)
-    df_DS_MG = xl_DS_MG.parse(xl_DS_MG.sheet_names[0])
-    df_DS_Hang = xl_DS_Hang.parse(xl_DS_Hang.sheet_names[0])
+    # Input Data MG, Hang KH
+    df_DS_MG = pd.read_excel("Data Môi Giới.xlsx")
+    df_DS_Hang = pd.read_excel("DS Hạng.xlsx")
+
 
     # Read file CF0079 & OD0024 with dataframe only
-    df_CF0079_Sheet1 = pd.read_excel("CF0079.xls", skiprows=[0,1,2,3], usecols=6, sheet_name='Sheet1')
-    df_CF0079_Sheet2 = pd.read_excel("CF0079.xls", skipfooter=3, usecols=6, sheet_name='Sheet2', header=None)
+    # CF0079
+    df_CF0079_Sheet1 = pd.read_excel(CF0079, skiprows=[0,1,2,3], usecols=6, sheet_name='Sheet1')
+    df_CF0079_Sheet2 = pd.read_excel(CF0079, skipfooter=3, usecols=6, sheet_name='Sheet2', header=None)
     df_CF0079 = pd.DataFrame(np.concatenate(
         [df_CF0079_Sheet1.values, df_CF0079_Sheet2.values]),
-                             columns=list(df_CF0079_Sheet1)
+                             columns=list(df_CF0079_Sheet1),
     )
+    df_CF0079 = df_CF0079.merge(df_DS_Hang,  how='left', on=['Hạng'])
 
-    df_OD0024 = pd.read_excel("OD0024.xls",
+    # OD0024
+    df_OD0024 = pd.read_excel(OD0024,
                               skiprows=[0],
                               skipfooter=2)
     df_OD0024 = df_OD0024[df_OD0024['Thuế '].notnull()]
 
+    # rename to set OD0024 and CF0079's header "Số TK' same variable
+    df_OD0024 = df_OD0024.rename(index=str, columns={'Số \ntài khoản': 'Số TK'})
     # merge OD with CF (Số Tk, MG chính, Hạng)
-    df_OD0024 = df_OD0024.merge(df_CF0079[['Số TK', 'MG chính', 'Hạng']],
-                                left_on='Số \ntài khoản',
-                                right_on='Số TK',
-                                how='left')
-
+    df_OD0024 = df_OD0024.merge(df_CF0079[['Số TK', 'MG chính', 'Hạng(Abv)', 'Hạng(Short)']], how='left', on=['Số TK'])
     # merge OD with DS MG
     df_OD0024 = pd.merge(df_OD0024, df_DS_MG, how='left', on=['MG chính'])
 
-    # classify KH FI to df_drop
-    df_OD0024_drop = df_OD0024[
-            (df_OD0024['MG chính'].notnull()) & (df_OD0024['Nhóm QL'].isnull())
-        ].copy()
-    df_OD0024_drop['Hạng(Abv)'] = 'Khác'
-    # retain others customer to df_0024
-    df_OD0024 = df_OD0024.drop(
-        df_OD0024[
-            (df_OD0024['MG chính'].notnull()) & (df_OD0024['Nhóm QL'].isnull())
-        ].index)
-
-    # merge OD24 with Hạng
-    df_OD0024 = pd.merge(df_OD0024, df_DS_Hang, how='left', on=['Hạng'])
-    # classify special customer class to df_drop_2
-    df_OD0024_drop_1 = df_OD0024[(df_OD0024['Hạng(Abv)'].isnull())].copy()
-    df_OD0024_drop_1['Hạng(Abv)'] = 'Khác'
-
-    # retain refined data of Brokerage Division
-    df_OD0024 = df_OD0024[df_OD0024['Hạng(Abv)'].notnull()].copy()
-
-    # concate and last refine data
-    df_OD0024 = df_OD0024.append([df_OD0024_drop, df_OD0024_drop_1], ignore_index=True)
-    df_OD0024 = df_OD0024.drop(['Số TK', 'Hạng', 'Nhóm QL'], axis=1)
+    # Set that all TK that have no Hạng(Abv) or Hạng(Short) are Khác
+    df_OD0024.fillna(value={'Hạng(Abv)': 'Khác', 'Hạng(Short)': 'Khác'}, inplace=True)
 
 
+
+    # Clear FI
+    df_OD0024['Hạng(Abv)'] = np.where((df_OD0024['MG chính'].notnull()) & (df_OD0024['Nhóm QL'].isnull()), 'Khác', df_OD0024['Hạng(Abv)'])
+    df_OD0024['Hạng(Short)'] = np.where((df_OD0024['MG chính'].notnull()) & (df_OD0024['Nhóm QL'].isnull()), 'Khác', df_OD0024['Hạng(Short)'])
+
+
+
+    # Speedup the speed by eliminate those whose inrelevant
+    df_CF0079_TVDT = df_CF0079.dropna(subset=['MG chính']).copy()
+
+    for i in ['TVĐTMM', 'TVĐTVIP', 'TVĐT']:
+        checkCondition = lambda row: len(df_CF0079_TVDT[
+                                                  (df_CF0079_TVDT['Hạng(Abv)'] == i) & (df_CF0079_TVDT['MG chính'] == row['MG chính'])
+                                              ])
+        head_name = 'Số khách ' + i
+        df_OD0024[head_name] = df_OD0024.apply(checkCondition, axis=1)
+
+
+    # Specify a writer
+    writer = pd.ExcelWriter('Result.xlsx', engine='xlsxwriter')
     # Write your DataFrame to a file
     df_OD0024.to_excel(writer, 'Sheet1')
     # Save the result
     writer.save()
-
 
 
 
